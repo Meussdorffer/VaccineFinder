81bcbc2608430c9f33d338e86d7158f09a6f3253
Jack Meussdorffer <jackcmv@gmail.com>
2021-04-02 16:43:03 -0700

    windows build automation

---
+++ make.bat
@@ -0,0 +1,13 @@
+@REM Delete old.
+del *.zip
+
+@REM Build layer.
+docker run --rm --volume=$(pwd):/lambda-build \
+-w=/lambda-build \
+lambci/lambda:build-python3.8 \
+pip install -r requirements.txt --target python
+
+
+@REM Build function.
+tar -acf lambda.zip lambda_function.py vacc.py
+tar -acf layer.zip python

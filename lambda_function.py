62b4338c34d57e995378c02bc1ae7cf5ae1982ef
Jack Meussdorffer <jackcmv@gmail.com>
2021-04-02 16:03:53 -0700

    lambda init

---
+++ lambda_function.py
@@ -0,0 +1,19 @@
+import json
+from vacc import find_vaccine
+
+ERR = {
+    'statusCode': 404,
+    'body': "Something went wrong, but I'm too lazy to tell you what it was."
+}
+
+def lambda_handler(event, context):
+    search = event.get('portland')
+    if not search:
+        return ERR
+
+    find_vaccine(search)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }

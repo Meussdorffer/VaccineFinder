9ff655c67377f56ac0bf7f461fdbf741a1d90cca
Jack Meussdorffer <jackcmv@gmail.com>
2021-04-02 18:04:33 -0700

    fix bat

--- make.bat
+++ make.bat
@@ -2,10 +2,7 @@
 del *.zip
 
 @REM Build layer.
-docker run --rm --volume=$(pwd):/lambda-build \
--w=/lambda-build \
-lambci/lambda:build-python3.8 \
-pip install -r requirements.txt --target python
+docker run --rm --volume=$(pwd):/lambda-build -w=/lambda-build lambci/lambda:build-python3.8 pip install -r requirements.txt --target python
 
 
 @REM Build function.
